c5567f83e60ae245b0ddcdfe82d8d71a5c08d3d6
Daniil <danya.moiseev@inbox.ru>
2021-04-26 01:44:00 +1000

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -38,4 +38,22 @@ $(document).ready(function(){
     $(".reviews-item__hidden-descr").fadeIn();
     $(".reviews-item__hidden-link").fadeOut();
   });
+
+  //Modal
+
+  $('[data-modal="consultation"]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow')
+  });
+
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow')
+  });
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
+
 });
